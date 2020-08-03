44124b88bf879b1ed410a7640e03ac63bf02cef0
Olga Skoroded <olga.skoroded@fullstack-development.com>
2020-08-03 14:53:37 +0700

    Install redux redux-saga, create store

---
+++ redux/store.ts
@@ -0,0 +1,20 @@
+import { applyMiddleware, createStore, Middleware } from 'redux';
+import createSagaMiddleware from 'redux-saga';
+import { createWrapper } from 'next-redux-wrapper';
+
+const bindMiddleware = (middleware) => {
+  if (process.env.NODE_ENV !== 'production') {
+    const { composeWithDevTools } = require('redux-devtools-extension');
+    return composeWithDevTools(applyMiddleware(...middleware));
+  }
+  return applyMiddleware(...middleware);
+};
+
+export const makeStore = (contex) => {
+  const sagaMiddleware = createSagaMiddleware();
+  const store = createStore(bindMiddleware([sagaMiddleware]));
+
+  return store;
+};
+
+export const wrapper = createWrapper(makeStore, { debug: true });
