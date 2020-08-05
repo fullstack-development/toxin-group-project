e903bde16ba285b77767547fa2b5ac800420fc91
Olga Skoroded <olga.skoroded@fullstack-development.com>
2020-08-05 14:15:07 +0700

    Add example of using

--- pages/guide/elements/index.tsx
+++ pages/guide/elements/index.tsx
@@ -1,3 +1,48 @@
-const Elements = () => <></>;
+import React from "react";
+import { Form } from "react-final-form";
+import Input from "../../../components/Input/Input";
+import styles from "./Elements.module.scss";
+import {
+  mustBeRequired,
+  mustBeEmail,
+  mustBeDate,
+  composeValidators,
+} from "../../../public/validators/validators";
+
+const onSubmit = () => {};
+
+const Elements = () => (
+  <div className={styles.elements}>
+    <div>
+      <Form
+        onSubmit={onSubmit}
+        render={({ handleSubmit, form, submitting, pristine, values }) => (
+          <form onSubmit={handleSubmit}>
+            <div className={styles.elements__input}>
+              <Input
+                name="date"
+                placeholder="ДД.ММ.ГГГГ"
+                type="text"
+                validate={composeValidators(mustBeRequired, mustBeDate)}
+                withMask
+                mask={[/\d/, /\d/, ".", /\d/, /\d/, ".", /\d/, /\d/, /\d/, /\d/]}
+                description={{ type: "masked text field", state: "default" }}
+              />
+            </div>
+            <div className={styles.elements__input}>
+              <Input
+                name="email"
+                placeholder="Email"
+                type="email"
+                validate={composeValidators(mustBeRequired, mustBeEmail)}
+                description={{ type: "text field", state: "default" }}
+              />
+            </div>
+          </form>
+        )}
+      />
+    </div>
+  </div>
+);
 
 export default Elements;
