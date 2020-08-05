bf28ad258a2d61712f227e5bfc97040cf5a80a59
Olga Skoroded <olga.skoroded@fullstack-development.com>
2020-08-05 20:40:34 +0700

    Remake input component without react-final-form

--- components/Input/Input.tsx
+++ components/Input/Input.tsx
@@ -1,5 +1,4 @@
 import MaskedInput from 'react-text-mask';
-import { Field } from 'react-final-form';
 import styles from './Input.module.scss';
 
 interface IInputDescription {
@@ -10,57 +9,53 @@ interface IInputDescription {
 interface IInputProps {
   name: string;
   placeholder: string;
-  validate?;
   type: string;
   withMask?: boolean;
   mask?: Array<any>;
   description: IInputDescription;
+  value: string;
+  touched: boolean;
+  onChange;
+  onBlur;
+  validator;
 }
 
-const Input: React.FC<IInputProps> = ({
-  name, placeholder, validate, type, withMask, mask, description,
-}) => (
-  <Field
-    name={name}
-    placeholder={placeholder}
-    type={type}
-    validate={validate}
-    withMask={withMask}
-    mask={mask}
-    description={description}
-  >
-    {({ input, meta, ...rest }) => (
-      <div className={styles.input}>
-        <div>
-          <div className={styles.input__description}>
-            <p className={styles['input__description-type']}>
-              {rest.description.type}
-            </p>
-            <p className={styles['input__description-state']}>
-              {rest.description.state}
-            </p>
-          </div>
-          {rest.withMask ? (
-            <MaskedInput
-              className={styles.input__field}
-              {...input}
-              placeholder={rest.placeholder}
-              mask={rest.mask}
-            />
-          ) : (
-            <input
-              className={styles.input__field}
-              {...input}
-              placeholder={rest.placeholder}
-            />
-          )}
-          {meta.error && meta.touched && (
-            <div className={styles.input__error}>{meta.error}</div>
-          )}
-        </div>
+const Input: React.FC<IInputProps> = ({name, placeholder, type, withMask, mask, description, value, onChange, onBlur, validator, touched}) => (
+  <div className={styles.input}>
+    <div>
+      <div className={styles.input__description}>
+        <p className={styles['input__description-type']}>
+          {description.type}
+        </p>
+        <p className={styles['input__description-state']}>
+          {description.state}
+        </p>
       </div>
-    )}
-  </Field>
+      {withMask ? (
+        <MaskedInput
+          className={styles.input__field}
+          placeholder={placeholder}
+          mask={mask}
+          type={type}
+          name={name}
+          value={value}
+          onChange={(e) => onChange(e)}
+          onBlur={(e) => onBlur(e)}
+        />
+      ) : (
+        <input
+          className={styles.input__field}
+          placeholder={placeholder}
+          type={type}
+          name={name}
+          value={value}
+          onChange={(e) => onChange(e)}
+          onBlur={(e) => onBlur(e)}
+        />
+      )}
+      { touched ? <div className={styles.input__error}>{validator(value)}</div> : null }
+    </div>
+  </div>
 );
 
 export default Input;
--- pages/guide/elements/index.tsx
+++ pages/guide/elements/index.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Form } from 'react-final-form';
 import Input from '../../../components/Input/Input';
 import styles from './Elements.module.scss';
 import {
@@ -9,42 +8,66 @@ import {
   composeValidators,
 } from '../../../public/validators/validators';
 
-const onSubmit = () => {};
+class Elements extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = ({ date: '', email: '', touched: { date: false, email: false } });
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleInputBlur = this.handleInputBlur.bind(this);
+  }
 
-const Elements = () => (
-  <div className={styles.elements}>
-    <div>
-      <Form
-        onSubmit={onSubmit}
-        render={({
-          handleSubmit, form, submitting, pristine, values,
-        }) => (
-          <form onSubmit={handleSubmit}>
-            <div className={styles.elements__input}>
-              <Input
-                name="date"
-                placeholder="ДД.ММ.ГГГГ"
-                type="text"
-                validate={composeValidators(mustBeRequired, mustBeDate)}
-                withMask
-                mask={[/\d/, /\d/, '.', /\d/, /\d/, '.', /\d/, /\d/, /\d/, /\d/]}
-                description={{ type: 'masked text field', state: 'default' }}
-              />
-            </div>
-            <div className={styles.elements__input}>
-              <Input
-                name="email"
-                placeholder="Email"
-                type="email"
-                validate={composeValidators(mustBeRequired, mustBeEmail)}
-                description={{ type: 'text field', state: 'default' }}
-              />
-            </div>
-          </form>
-        )}
-      />
-    </div>
-  </div>
-);
+  handleInputChange(e) {
+    const target = e.target;
+
+    this.setState({
+      [target.name]: target.value,
+    });
+  }
+
+  handleInputBlur(e) {
+    const target = e.target;
+
+    this.setState({
+      touched: { ...this.state.touched, [target.name]: true },
+    });
+  }
+
+  render() {
+    return (
+      <div className={styles.elements}>
+        <form>
+          <div className={styles.elements__input}>
+            <Input
+              name="date"
+              placeholder="ДД.ММ.ГГГГ"
+              type="text"
+              withMask
+              mask={[/\d/, /\d/, '.', /\d/, /\d/, '.', /\d/, /\d/, /\d/, /\d/]}
+              description={{ type: 'masked text field', state: 'default' }}
+              value={this.state.date}
+              onChange={this.handleInputChange}
+              onBlur={this.handleInputBlur}
+              validator={composeValidators(mustBeRequired, mustBeDate)}
+              touched={this.state.touched.date}
+            />
+          </div>
+          <div className={styles.elements__input}>
+            <Input
+              name="email"
+              placeholder="Email"
+              type="email"
+              description={{ type: 'text field', state: 'default' }}
+              value={this.state.email}
+              onChange={this.handleInputChange}
+              onBlur={this.handleInputBlur}
+              validator={composeValidators(mustBeRequired, mustBeEmail)}
+              touched={this.state.touched.email}
+            />
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
 
 export default Elements;
