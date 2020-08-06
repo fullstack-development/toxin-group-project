cf6167492f875d833169c42b9f2bcab58d3b60f6
Olga Skoroded <olga.skoroded@fullstack-development.com>
2020-08-06 16:00:25 +0700

    Add input validation

--- pages/guide/elements/index.tsx
+++ pages/guide/elements/index.tsx
@@ -5,8 +5,31 @@ import styles from './Elements.module.scss';
 class Elements extends React.Component {
   constructor(props) {
     super(props);
-    this.state = ({ date: '', email: '' });
+    this.state = {
+      name: '',
+      email: '',
+      formErrors: { email: '', name: '' },
+    };
     this.handleInputChange = this.handleInputChange.bind(this);
+  }
+
+  validateField(fieldName, value) {
+    const fieldValidationErrors = this.state.formErrors;
+
+    switch (fieldName) {
+      case 'email':
+        const emailValid = value && value.match(/^([\w.%+-]+)@([\w-]+\.)+([\w]{2,})$/i);
+        fieldValidationErrors.email = emailValid ? '' : 'Некорректный email';
+        break;
+      case 'name':
+        const nameValid = value.match(/^[a-zA-ZА-Яа-я]+$/);
+        fieldValidationErrors.name = nameValid ? '' : 'Имя может содеражть только буквенные символы';
+        break;
+      default:
+        break;
+    }
+
+    this.setState({ formErrors: fieldValidationErrors }, this.validateForm);
   }
 
   handleInputChange(e) {
@@ -14,7 +37,7 @@ class Elements extends React.Component {
 
     this.setState({
       [target.name]: target.value,
-    });
+    }, () => { this.validateField(target.name, target.value) });
   }
 
   render() {
@@ -23,21 +46,23 @@ class Elements extends React.Component {
         <form>
           <div className={styles.elements__input}>
             <Input
-              name="date"
-              placeholder="ДД.ММ.ГГГГ"
-              type="text"
-              value={this.state.date}
+              name='name'
+              placeholder='Name'
+              type='text'
+              value={this.state.name}
               onChange={this.handleInputChange}
             />
+            <div className={styles['elements__input-error']}>{this.state.formErrors.name}</div>
           </div>
           <div className={styles.elements__input}>
             <Input
-              name="email"
-              placeholder="Email"
-              type="email"
+              name='email'
+              placeholder='Email'
+              type='email'
               value={this.state.email}
               onChange={this.handleInputChange}
             />
+            <div className={styles['elements__input-error']}>{this.state.formErrors.email}</div>
           </div>
         </form>
       </div>
