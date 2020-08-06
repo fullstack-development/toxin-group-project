20cec51c24a3731d274255f55ab3b244fa827f21
Olga Skoroded <olga.skoroded@fullstack-development.com>
2020-08-06 13:10:10 +0700

    Add simple imput component

--- components/Input/Input.tsx
+++ components/Input/Input.tsx
@@ -1,61 +1,17 @@
-import MaskedInput from 'react-text-mask';
 import styles from './Input.module.scss';
 
-interface IInputDescription {
-  type: string;
-  state: string;
-}
-
-interface IInputProps {
-  name: string;
-  placeholder: string;
-  type: string;
-  withMask?: boolean;
-  mask?: Array<any>;
-  description: IInputDescription;
-  value: string;
-  touched: boolean;
-  onChange;
-  onBlur;
-  validator;
-}
-
-const Input: React.FC<IInputProps> = ({name, placeholder, type, withMask, mask, description, value, onChange, onBlur, validator, touched}) => (
-  <div className={styles.input}>
-    <div>
-      <div className={styles.input__description}>
-        <p className={styles['input__description-type']}>
-          {description.type}
-        </p>
-        <p className={styles['input__description-state']}>
-          {description.state}
-        </p>
-      </div>
-      {withMask ? (
-        <MaskedInput
-          className={styles.input__field}
-          placeholder={placeholder}
-          mask={mask}
-          type={type}
-          name={name}
-          value={value}
-          onChange={(e) => onChange(e)}
-          onBlur={(e) => onBlur(e)}
-        />
-      ) : (
-        <input
-          className={styles.input__field}
-          placeholder={placeholder}
-          type={type}
-          name={name}
-          value={value}
-          onChange={(e) => onChange(e)}
-          onBlur={(e) => onBlur(e)}
-        />
-      )}
-      { touched ? <div className={styles.input__error}>{validator(value)}</div> : null }
-    </div>
-  </div>
+const Input: React.FC = ({
+  name, placeholder, type, value, onChange, ...rest
+}) => (
+  <input
+    className={styles.input}
+    placeholder={placeholder}
+    type={type}
+    name={name}
+    value={value}
+    onChange={(e) => onChange(e)}
+    {...rest}
+  />
 );
 
 export default Input;
--- pages/guide/elements/index.tsx
+++ pages/guide/elements/index.tsx
@@ -1,34 +1,19 @@
 import React from 'react';
 import Input from '../../../components/Input/Input';
 import styles from './Elements.module.scss';
-import {
-  mustBeRequired,
-  mustBeEmail,
-  mustBeDate,
-  composeValidators,
-} from '../../../public/validators/validators';
 
 class Elements extends React.Component {
   constructor(props) {
     super(props);
-    this.state = ({ date: '', email: '', touched: { date: false, email: false } });
+    this.state = ({ date: '', email: '' });
     this.handleInputChange = this.handleInputChange.bind(this);
-    this.handleInputBlur = this.handleInputBlur.bind(this);
   }
 
   handleInputChange(e) {
-    const target = e.target;
+    const { target } = e;
 
     this.setState({
       [target.name]: target.value,
-    });
-  }
-
-  handleInputBlur(e) {
-    const target = e.target;
-
-    this.setState({
-      touched: { ...this.state.touched, [target.name]: true },
     });
   }
 
@@ -41,14 +26,8 @@ class Elements extends React.Component {
               name="date"
               placeholder="ДД.ММ.ГГГГ"
               type="text"
-              withMask
-              mask={[/\d/, /\d/, '.', /\d/, /\d/, '.', /\d/, /\d/, /\d/, /\d/]}
-              description={{ type: 'masked text field', state: 'default' }}
               value={this.state.date}
               onChange={this.handleInputChange}
-              onBlur={this.handleInputBlur}
-              validator={composeValidators(mustBeRequired, mustBeDate)}
-              touched={this.state.touched.date}
             />
           </div>
           <div className={styles.elements__input}>
@@ -56,12 +35,8 @@ class Elements extends React.Component {
               name="email"
               placeholder="Email"
               type="email"
-              description={{ type: 'text field', state: 'default' }}
               value={this.state.email}
               onChange={this.handleInputChange}
-              onBlur={this.handleInputBlur}
-              validator={composeValidators(mustBeRequired, mustBeEmail)}
-              touched={this.state.touched.email}
             />
           </div>
         </form>
