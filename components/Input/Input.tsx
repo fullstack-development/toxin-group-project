2d2dbe2828e3c310713d503a13801f77dac39056
Olga Skoroded <olga.skoroded@fullstack-development.com>
2020-08-07 09:48:59 +0700

    Put rest props before base props

--- components/Input/Input.tsx
+++ components/Input/Input.tsx
@@ -4,13 +4,13 @@ const Input: React.FC = ({
   name, placeholder, type, value, onChange, ...rest
 }) => (
   <input
+    {...rest}
     className={styles.input}
     placeholder={placeholder}
     type={type}
     name={name}
     value={value}
     onChange={(e) => onChange(e)}
-    {...rest}
   />
 );
 
