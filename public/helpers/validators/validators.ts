79325e53821f420099b2b58bac436e5952bb3139
Olga Skoroded <olga.skoroded@fullstack-development.com>
2020-08-11 15:12:00 +0700

    Add types to fieldValidator

--- public/helpers/validators/validators.ts
+++ public/helpers/validators/validators.ts
@@ -1,4 +1,4 @@
-export const fieldValidator = (fieldName, value) => {
+export const fieldValidator = (fieldName: string, value: string) => {
   switch (fieldName) {
     case 'email':
       const emailValid = value.match(/^([\w.%+-]+)@([\w-]+\.)+([\w]{2,})$/i);
