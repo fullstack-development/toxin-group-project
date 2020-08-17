fd10df6dad6e9a3b4dba3d92444ca66a304a7601
Olga Skoroded <olga.skoroded@fullstack-development.com>
2020-08-17 12:33:36 +0700

    Add ThemeProvider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,14 @@
-import App from 'next/app';
-import '../styles/styles.scss';
+import { ThemeProvider } from 'styled-components';
+import GlobalStyle from 'shared/styles/styles';
+import purpleThemePalette from 'shared/styles/theme';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <ThemeProvider theme={purpleThemePalette}>
+      <Component {...pageProps} />
+      <GlobalStyle />
+    </ThemeProvider>
+  )
 }
 
 export default MyApp;
