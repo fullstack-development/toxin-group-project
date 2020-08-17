15612b7eb511cbb4a5a8085c30ef973bc6962014
Olga Skoroded <olga.skoroded@fullstack-development.com>
2020-08-17 12:27:15 +0700

    Create theme palette

---
+++ shared/styles/theme.ts
@@ -0,0 +1,22 @@
+const typographyColorValue = '31, 32, 65';
+const primaryColorValue = '188, 156, 255';
+const secondaryColorValue = '111, 207, 151';
+
+const purpleThemePalette = {
+  typography: {
+    colorLightest: `RGBA(${typographyColorValue}, 5%)`,
+    colorLight: `RGBA(${typographyColorValue}, 25%)`,
+    color: `RGBA(${typographyColorValue}, 50%)`,
+    colorDark: `RGBA(${typographyColorValue}, 75%)`,
+    colorDarkest: `RGB(${typographyColorValue})`,
+    fontName: 'Montserrat',
+    typographyFontSize: '14px',
+  },
+  colors: {
+    primary: `RGB(${primaryColorValue})`,
+    secondary: `RGB(${secondaryColorValue})`,
+  },
+  defaultBackground: '#fff',
+};
+
+export default purpleThemePalette;
