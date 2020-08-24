e11fb96e07b7c971601d0330c0ed52b9db29f6d7
AkhmadBabaev <akhmad.babaev@gmail.com>
2020-08-24 17:22:10 +0300

    Get rid of useless methods of Api class

--- utils/api.ts
+++ utils/api.ts
@@ -1,34 +1,10 @@
-import { boundMethod } from 'autobind-decorator';
-
 import Apartments from './entities/Apartments';
-import firebase from './firebase';
-
-type DataSnapshot = firebase.database.DataSnapshot;
 
 class Api {
   public apartments: Apartments;
 
   constructor() {
     this.apartments = new Apartments();
-  }
-
-  @boundMethod
-  public request(
-    value: string,
-    callback: (a: DataSnapshot, b?: string) => any,
-    watch = true,
-  ): ((a: DataSnapshot, b?: string) => any) | Promise<DataSnapshot> {
-    return firebase.request(value, callback, watch);
-  }
-
-  @boundMethod
-  public post(field: string, data: any): void {
-    firebase.post(field, data);
-  }
-
-  @boundMethod
-  public remove(field: string): void {
-    firebase.remove(field);
   }
 }
 
