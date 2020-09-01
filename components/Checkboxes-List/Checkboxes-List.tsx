18693f28125b08768af779a025ccca354fd84b3d
Olga Skoroded <olga.skoroded@fullstack-development.com>
2020-09-01 11:33:53 +0500

    Make Checkbox List stateless component

--- components/Checkboxes-List/Checkboxes-List.tsx
+++ components/Checkboxes-List/Checkboxes-List.tsx
@@ -15,40 +15,25 @@ type Props = {
   roomOptions: Array<Option>;
 };
 
-class CheckboxesList extends React.Component<Props> {
-  state = {};
-
-  handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const { value, checked } = e.target;
-
-    this.setState({
-      [value]: checked,
-    });
-  };
-
-  render() {
-    const { roomOptions } = this.props;
-    return (
-      <List>
-        {roomOptions.map((option) => (
-          <ListItem key={option.value}>
-            <Field
-              type="checkbox"
-              name={option.name}
-              value={option.value}
-              render={(props) => (
-                <Checkbox
-                  label={option.label}
-                  {...props.input}
-                  {...props.meta}
-                />
-              )}
+const CheckboxesList: React.FC<Props> = ({ roomOptions }: Props) => (
+  <List>
+    {roomOptions.map((option) => (
+      <ListItem key={option.value}>
+        <Field
+          type="checkbox"
+          name={option.name}
+          value={option.value}
+          render={(props) => (
+            <Checkbox
+              label={option.label}
+              {...props.input}
+              {...props.meta}
             />
-          </ListItem>
-        ))}
-      </List>
-    );
-  }
-}
+          )}
+        />
+      </ListItem>
+    ))}
+  </List>
+);
 
 export default CheckboxesList;
