cf8b65d55b325f0962136f5aa03885257d3ed8ec
Frostmourne <cozroman@yandex.ru>
2020-09-04 14:35:39 +0300

    fix: move all exports to the end of the file

--- pages/guide/elements/elements.styles.ts
+++ pages/guide/elements/elements.styles.ts
@@ -1,19 +1,23 @@
 import styled from 'styled-components';
 
-export const Container = styled.div`
+const Container = styled.div`
   padding: 3rem;
 `;
 
-export const InputWrapper = styled.div`
+const InputWrapper = styled.div`
   width: 22.8571rem;
   margin: 1rem 0;
 `;
 
-export const ErrorMessage = styled.div`
+const ErrorMessage = styled.div`
   font-size: ${(props) => props.theme.error.fontSize};
   color: ${(props) => props.theme.error.color};
 `;
 
-export const BulletListWrapper = styled.div`
+const BulletListWrapper = styled.div`
   max-width: 18.5714rem;
 `;
+
+export {
+  Container, InputWrapper, ErrorMessage, BulletListWrapper,
+};
