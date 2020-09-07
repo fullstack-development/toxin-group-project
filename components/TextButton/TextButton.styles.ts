42ecb7603ac8518490b5a050b3d72fdef2392fbb
Frostmourne <cozroman@yandex.ru>
2020-09-07 16:43:19 +0300

    fix: font fix

--- components/TextButton/TextButton.styles.ts
+++ components/TextButton/TextButton.styles.ts
@@ -2,9 +2,10 @@ import styled, { css } from 'styled-components';
 
 const TextButton = styled.button`
 ${(props) => {
-    const { colors: { primary }, typography: { color } } = props.theme;
+    const { colors: { primary }, typography: { color, fontName } } = props.theme;
     return css`
       text-transform: uppercase;
+      font-family: ${fontName};
       font-size: 12px;
       font-weight: 700;
       background-color: transparent;
