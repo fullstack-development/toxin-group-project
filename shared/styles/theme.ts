d98da95588eeb0015fe302082fb561d758d9e420
Olga Skoroded <olga.skoroded@fullstack-development.com>
2020-09-08 20:41:10 +0700

    Improve names for theme settings

--- shared/styles/theme.ts
+++ shared/styles/theme.ts
@@ -2,26 +2,31 @@ import { Theme } from '../types/theme';
 
 const typographyColorValue = '31, 32, 65';
 const primaryColorValue = '188, 156, 255';
+const primaryVariantColorValue = '139, 164, 249';
 const secondaryColorValue = '111, 207, 151';
+const secondaryVariantColorValue = '102, 210, 234';
 
 const purpleThemePalette: Theme = {
   typography: {
-    colorLightest: `RGBA(${typographyColorValue}, 5%)`,
-    colorLight: `RGBA(${typographyColorValue}, 25%)`,
-    color: `RGBA(${typographyColorValue}, 50%)`,
-    colorDark: `RGBA(${typographyColorValue}, 75%)`,
-    colorDarkest: `RGB(${typographyColorValue})`,
     fontName: 'Montserrat',
     fontSize: '14px',
   },
   colors: {
     primary: `RGB(${primaryColorValue})`,
+    primaryVariant: `RGB(${primaryVariantColorValue})`,
     secondary: `RGB(${secondaryColorValue})`,
+    secondaryVariant: `RGB(${secondaryVariantColorValue})`,
+    basicLightest: `RGBA(${typographyColorValue}, 5%)`,
+    basicLight: `RGBA(${typographyColorValue}, 25%)`,
+    basic: `RGBA(${typographyColorValue}, 50%)`,
+    basicDark: `RGBA(${typographyColorValue}, 75%)`,
+    basicDarkest: `RGB(${typographyColorValue})`,
+    error: 'RGB(244, 67, 54)',
+    defaultBackground: 'RGB(255, 255, 255)',
   },
-  defaultBackground: '#fff',
-  error: {
-    color: '#f44336',
-    fontSize: '0.8571rem',
+  gradients: {
+    primary: `linear-gradient(180deg, ${primaryColorValue} 0%, ${primaryVariantColorValue} 100%)`,
+    secondary: `linear-gradient(180deg, ${secondaryColorValue} 0%, ${secondaryVariantColorValue} 100%)`,
   },
 };
 
