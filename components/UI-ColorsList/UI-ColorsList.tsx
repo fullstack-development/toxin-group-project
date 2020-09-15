ced9bf9a0ac05e3954ca73bee93bfc17c926a2b8
Olga Skoroded <olga.skoroded@fullstack-development.com>
2020-09-15 16:54:34 +0700

    Add colors list

---
+++ components/UI-ColorsList/UI-ColorsList.tsx
@@ -0,0 +1,37 @@
+import UIColor from 'components/UI-Color/UI-Color';
+
+import * as S from './UI-ColorsList.styles';
+
+type Color = {
+  color: string;
+  title: string;
+  opacity: number;
+  id: number
+};
+
+type Props = {
+  colors: Array<Color>;
+}
+
+const UIColorsList: React.FC<Props> = (props: Props) => {
+  const { colors } = props;
+
+  return (
+    <section>
+      <S.Title>Цветовая палитра</S.Title>
+      <S.List lang="en">
+        {colors.map((color) => (
+          <li key={color.id}>
+            <UIColor
+              color={color.color}
+              title={color.title}
+              opacity={color.opacity}
+            />
+          </li>
+        ))}
+      </S.List>
+    </section>
+  );
+};
+
+export default UIColorsList;
