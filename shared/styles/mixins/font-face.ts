dcb051ae813ab8b7f4d1f044296a06a47180aaaf
Ilanevazno <ilya.sigidin@fullstack-development.com>
2020-09-15 22:03:29 +0300

    moved fonts

--- shared/styles/mixins/font-face.ts
+++ shared/styles/mixins/font-face.ts
@@ -2,8 +2,8 @@ export default function fontFace(fontFamily, src, fontWeight = 400, fontStyle = 
   return `
     @font-face{
       font-family: '${fontFamily}';
-      src: url('/fonts/${src}.woff2') format('woff2'),
-          url('/fonts/${src}.woff') format('woff');
+      src: url('/fonts/${fontFamily}/${src}.woff2') format('woff2'),
+          url('/fonts/${fontFamily}/${src}.woff') format('woff');
       font-style: ${fontStyle};
       font-weight: ${fontWeight};
     }
