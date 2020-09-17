0b9365f27184d8b9eafe106860707b9c1d5c154a
Olga Skoroded <olga.skoroded@fullstack-development.com>
2020-09-17 12:03:06 +0700

    Fix container styles

--- shared/styles/mixins/container.ts
+++ shared/styles/mixins/container.ts
@@ -3,8 +3,10 @@ import { css } from 'styled-components';
 const container = css`
   min-width: 320px;
   max-width: 1920px;
-  padding: 0 1rem;
-  margin: 0 auto;
+  padding-left: 1rem;
+  padding-right: 1rem;
+  margin-left: auto;
+  margin-right: auto;
 `;
 
 export default container;
