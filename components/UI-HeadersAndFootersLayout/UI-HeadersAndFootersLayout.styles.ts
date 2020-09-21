d7638b2659e13bc5126f3ec2e5840eb610470539
Ilanevazno <ilya.sigidin@fullstack-development.com>
2020-09-21 18:57:07 +0300

    created layout

---
+++ components/UI-HeadersAndFootersLayout/UI-HeadersAndFootersLayout.styles.ts
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+const Container = styled.div`
+  max-width: 102.8rem;
+  padding: 10rem 0;
+  margin: auto;
+`;
+
+export { Container };
---
+++ components/UI-HeadersAndFootersLayout/UI-HeadersAndFootersLayout.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+import * as S from './UI-HeadersAndFootersLayout.styles';
+
+const UIHeadersAndFootersLayout: React.FC = (): JSX.Element => <S.Container />;
+
+export default UIHeadersAndFootersLayout;
