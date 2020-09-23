6804f7346be86823e83d3248bc427b8043297bb0
Ilanevazno <ilya.sigidin@fullstack-development.com>
2020-09-23 15:38:45 +0300

    moved data to json file

--- components/UI-HeadersAndFootersLayout/UI-HeadersAndFootersLayout.tsx
+++ components/UI-HeadersAndFootersLayout/UI-HeadersAndFootersLayout.tsx
@@ -1,36 +1,13 @@
 import React from 'react';
 
 import NavMenu from 'components/NavMenu/NavMenu';
+import NavLinks from 'components/NavMenu/NavMenu.data.json';
 
 import * as S from './UI-HeadersAndFootersLayout.styles';
 
 const UIHeadersAndFootersLayout: React.FC = (): JSX.Element => (
   <S.Container>
-    <NavMenu
-      menu={[
-        { name: 'О нас', path: '/mock-about-us', isActive: true },
-        {
-          name: 'Услуги',
-          path: '/mock-services',
-          subMenu: [
-            { name: 'sub-услуги', path: '/mock-sub-services' },
-            { name: 'sub-услуги', path: '/mock-sub-services' },
-            { name: 'sub-услуги', path: '/mock-sub-services' },
-          ],
-        },
-        { name: 'Вакансии', path: '/mock-vacancies' },
-        { name: 'Новости', path: '/mock-news' },
-        {
-          name: 'Соглашения',
-          path: '/mock-agreements',
-          subMenu: [
-            { name: 'sub-соглашения', path: '/mock-sub-agreements' },
-            { name: 'sub-соглашения', path: '/mock-sub-agreements' },
-            { name: 'sub-соглашения', path: '/mock-sub-agreements' },
-          ],
-        },
-      ]}
-    />
+    <NavMenu menu={NavLinks} />
   </S.Container>
 );
 
