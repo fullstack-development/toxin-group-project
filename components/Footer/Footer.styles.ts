a11d9a0badbf3752a020d07f70eed7bf4b683d0e
Frostmourne <cozroman@yandex.ru>
2020-09-24 17:32:34 +0300

    fix: padding fix

--- components/Footer/Footer.styles.ts
+++ components/Footer/Footer.styles.ts
@@ -4,10 +4,12 @@ import { container, visuallyHidden } from 'shared/styles/mixins';
 
 const Container = styled.div`
   ${container}
-  padding: 7.1429rem 10rem;
+  padding-left: 10rem;
+  padding-right: 10rem;
 
   @media (max-width: 1200px) {
-    padding: 7.1429rem 4.2857rem;
+    padding-left: 4.2857rem;
+    padding-right: 4.2857rem;
   }
 `;
 
@@ -16,6 +18,8 @@ const MainContainer = styled(Container)`
   grid-template-columns: minmax(0, 18.5714rem) 1fr minmax(0, 18.5714rem);
   grid-gap: 40px;
   border-bottom: 0.0714rem solid rgba(31, 32, 65, 0.1);
+  padding-top: 7.1429rem;
+  padding-bottom: 7.1429rem;
 
   @media (max-width: 900px) {
     display: flex;
