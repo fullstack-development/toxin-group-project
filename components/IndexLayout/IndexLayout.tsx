ae53597e3b5075666ae1848dc65a7df7bb7249b0
AkhmadBabaev <akhmad.babaev@gmail.com>
2020-09-24 19:17:55 +0300

    Create banner

---
+++ components/IndexLayout/IndexLayout.tsx
@@ -0,0 +1,5 @@
+import Banner from './components/Banner/Banner';
+
+const IndexLayout: React.FC = (): JSX.Element => <Banner />;
+
+export default IndexLayout;
---
+++ components/IndexLayout/components/Banner/Banner.tsx
@@ -0,0 +1,9 @@
+import * as S from './Banner.styles';
+
+const Banner = (): JSX.Element => (
+  <S.Banner>
+    <S.Message>Лучшие номера для вашей работы, отдыха и просто вдохновения</S.Message>
+  </S.Banner>
+);
+
+export default Banner;
