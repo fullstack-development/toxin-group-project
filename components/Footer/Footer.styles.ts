63fb73f14154f5aad1be53c5c96344e1bf89d2f5
Frostmourne <cozroman@yandex.ru>
2020-09-25 09:30:31 +0300

    fix: add border color to the theme, remove redundant code

--- components/Footer/Footer.styles.ts
+++ components/Footer/Footer.styles.ts
@@ -1,4 +1,4 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
 import { container, visuallyHidden } from 'shared/styles/mixins';
 
@@ -14,24 +14,29 @@ const Container = styled.div`
 `;
 
 const MainContainer = styled(Container)`
-  display: grid;
-  grid-template-columns: minmax(0, 18.5714rem) 1fr minmax(0, 18.5714rem);
-  grid-gap: 2.8571rem;
-  border-bottom: 0.0714rem solid rgba(31, 32, 65, 0.1);
-  padding-top: 7.1429rem;
-  padding-bottom: 7.1429rem;
+  ${(props) => {
+    const { colors } = props.theme;
+    return css`
+      display: grid;
+      grid-template-columns: minmax(0, 18.5714rem) 1fr minmax(0, 18.5714rem);
+      grid-gap: 2.8571rem;
+      border-bottom: 0.0714rem solid ${colors.basicPale};
+      padding-top: 7.1429rem;
+      padding-bottom: 7.1429rem;
 
-  @media (max-width: 900px) {
-    display: flex;
-    justify-content: center;
-    padding-bottom: 0;
-    padding-top: 8.5714rem;
-    border-bottom: 0;
-  }
+      @media (max-width: 900px) {
+        display: flex;
+        justify-content: center;
+        padding-bottom: 0;
+        padding-top: 8.5714rem;
+        border-bottom: 0;
+      }
 
-  @media (max-width: 500px) {
-    padding-top: 3.5714rem;
-  }
+      @media (max-width: 500px) {
+        padding-top: 3.5714rem;
+      }
+    `;
+  }}
 `;
 
 const Title = styled.h2`
--- components/UI-HeadersAndFootersLayout/UI-HeadersAndFootersLayout.styles.ts
+++ components/UI-HeadersAndFootersLayout/UI-HeadersAndFootersLayout.styles.ts
@@ -6,9 +6,4 @@ const Container = styled.div`
   ${container};
 `;
 
-const FooterContainer = styled(Container)`
-  padding: 7.1429rem 10rem;
-  border-bottom: 0.0714rem solid rgba(31, 32, 65, 0.1);
-`;
-
-export { Container, FooterContainer };
+export { Container };
