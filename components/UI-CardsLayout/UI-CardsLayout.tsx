089cc4f8c2cbca2d3f6e52ba697960caeb74f3fc
Olga Skoroded <olga.skoroded@fullstack-development.com>
2020-09-25 16:42:54 +0700

    Add empty cards page

---
+++ components/UI-CardsLayout/UI-CardsLayout.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+import * as S from './UI-CardsLayout.styles';
+
+const UICardsLayout: React.FC = (): JSX.Element => <S.Container />;
+
+export default UICardsLayout;
---
+++ pages/guide/cards.tsx
@@ -0,0 +1,5 @@
+import UICardsLayout from 'components/UI-CardsLayout/UI-CardsLayout';
+
+const Cards: React.FC = (): JSX.Element => <UICardsLayout />;
+
+export default Cards;
