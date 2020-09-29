db83174156d08d53630fa34e5553f935f5b5dd79
Frostmourne <cozroman@yandex.ru>
2020-09-29 14:56:14 +0300

    feat: add arrow buttons

--- components/Room/components/ImageGallery/ImageGallery.styles.ts
+++ components/Room/components/ImageGallery/ImageGallery.styles.ts
@@ -1,11 +1,8 @@
 import styled, { css } from 'styled-components';
 
-import { ImageProps } from '../../Room.types';
+import { materialIcons } from 'shared/styles/mixins';
 
-const Container = styled.div`
-  position: relative;
-  min-height: 10.7857rem;
-`;
+import { ImageProps } from '../../Room.types';
 
 const Img = styled.img<ImageProps>`
   ${(props) => {
@@ -27,6 +24,7 @@ const Img = styled.img<ImageProps>`
 const Dots = styled.ul`
   display: flex;
   position: absolute;
+  z-index: 10;
   right: 1.0714rem;
   bottom: 1.0714rem;
 `;
@@ -39,4 +37,71 @@ const Dot = styled.li`
   }
 `;
 
-export { Container, Img, Dots, Dot };
+const ArrowContainer = styled.div`
+  display: none;
+  position: absolute;
+  width: 3rem;
+  height: 100%;
+  z-index: 5;
+`;
+
+const ArrowPrevContainer = styled(ArrowContainer)`
+  left: 0;
+  background-image: linear-gradient(90deg, rgba(0, 0, 0, 0.5), rgba(255, 255, 255, 0));
+`;
+
+const ArrowNextContainer = styled(ArrowContainer)`
+  right: 0;
+  background-image: linear-gradient(-90deg, rgba(0, 0, 0, 0.5), rgba(255, 255, 255, 0));
+`;
+
+const Container = styled.div`
+  position: relative;
+  min-height: 10.7857rem;
+
+  &:hover ${ArrowContainer} {
+    display: block;
+  }
+
+  @media (max-width: 1050px) {
+    ${ArrowContainer} {
+      display: block;
+    }
+  }
+`;
+
+const ArrowButton = styled.button`
+  ${materialIcons};
+  border: 0;
+  background-color: white;
+  font-size: 1.7rem;
+  position: absolute;
+  left: 0.73rem;
+  top: 50%;
+  transform: translateY(-50%);
+  z-index: 1;
+  cursor: pointer;
+`;
+
+const ArrowButtonNext = styled(ArrowButton)`
+  &::before {
+    content: 'keyboard_arrow_right';
+  }
+`;
+
+const ArrowButtonPrev = styled(ArrowButton)`
+  &::before {
+    content: 'keyboard_arrow_left';
+  }
+`;
+
+export {
+  Container,
+  Img,
+  Dots,
+  Dot,
+  ArrowPrevContainer,
+  ArrowNextContainer,
+  ArrowButtonNext,
+  ArrowButtonPrev,
+};
--- components/Room/components/ImageGallery/ImageGallery.tsx
+++ components/Room/components/ImageGallery/ImageGallery.tsx
@@ -4,6 +4,8 @@ import { GalleryProps } from '../../Room.types';
 import DotButton from '../DotButton/DotButton';
 import * as S from './ImageGallery.styles';
 
+const defaultState = [false, false, false, false];
+
 const ImageGallery: React.FC<GalleryProps> = ({
   imagePaths = ['/img/room-1.jpg', '/img/room-2.jpg', '/img/room-3.jpg', '/img/room-4.jpg'],
 }: GalleryProps) => {
@@ -11,13 +13,31 @@ const ImageGallery: React.FC<GalleryProps> = ({
 
   const handleDotClick = (e: React.MouseEvent<HTMLButtonElement>) => {
     const { index } = e.currentTarget.dataset;
-    const state = [false, false, false, false];
+    const state = [...defaultState];
     state[index] = true;
     setImageStates(state);
   };
 
+  const makeArrowClickHandler = (increment) => () => {
+    setImageStates((prevState) => {
+      const shownImageIndex = prevState.indexOf(true);
+      const incrementedIndex = shownImageIndex + increment;
+      const newIndex =
+        incrementedIndex < 0 ? prevState.length - 1 : incrementedIndex % prevState.length;
+      const state = [...defaultState];
+      state[newIndex] = true;
+      return state;
+    });
+  };
+
+  const handleArrowPrevClick = makeArrowClickHandler(-1);
+  const handleArrowNextClick = makeArrowClickHandler(1);
+
   return (
     <S.Container>
+      <S.ArrowPrevContainer>
+        <S.ArrowButtonPrev aria-label="Назад" onClick={handleArrowPrevClick} />
+      </S.ArrowPrevContainer>
       {imagePaths.map((path, index) => (
         <S.Img key={path + String(index)} src={path} isShown={imageStates[index]} />
       ))}
@@ -36,6 +56,9 @@ const ImageGallery: React.FC<GalleryProps> = ({
           );
         })}
       </S.Dots>
+      <S.ArrowNextContainer>
+        <S.ArrowButtonNext aria-label="Вперед" onClick={handleArrowNextClick} />
+      </S.ArrowNextContainer>
     </S.Container>
   );
 };
