8cc30054cc9f6072e4fcd002ff714ccfee68148b
Ilanevazno <ilya.sigidin@fullstack-development.com>
2020-10-01 11:29:44 +0300

    adaptive fixes

--- components/RegistrationLayout/components/InnerPage/InnerPage.styles.ts
+++ components/RegistrationLayout/components/InnerPage/InnerPage.styles.ts
@@ -1,31 +1,25 @@
 import styled from 'styled-components';
 
-import { container } from 'shared/styles/mixins';
-
 const Container = styled.div`
-  ${container}
   background: url('./img/auth-register.jpg') bottom / cover no-repeat;
-  min-height: 59.286rem;
-  padding: 5rem 9.6rem 1.47rem 10rem;
+  min-height: 40rem;
+  padding: 2rem;
+
+  & > form {
+    margin: 0 auto;
+  }
 
   @media (max-width: 1200px) {
-    min-height: 48rem;
-    padding: 4rem 8rem 1.47rem;
+    min-height: 30rem;
+    padding: 1rem;
   }
 
   @media (max-width: 992px) {
-    min-height: 40rem;
-    padding: 3rem 6rem 1.47rem;
-  }
-
-  @media (max-width: 768px) {
-    min-height: 35rem;
-    padding: 2rem 4rem 1rem;
+    min-height: 20rem;
   }
 
   @media (max-width: 576px) {
-    min-height: 30rem;
-    padding: 1rem;
+    min-height: 10rem;
   }
 `;
 
