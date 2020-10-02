8be83d09c70dd1bccf52301e15d4acddd01d3d8c
Frostmourne <cozroman@yandex.ru>
2020-10-02 14:26:29 +0300

    refactor: add react  infinite scroll component

--- components/Rooms/Rooms.tsx
+++ components/Rooms/Rooms.tsx
@@ -12,69 +12,50 @@ export type RoomsProps = {
   rooms: RoomProps[];
 };
 
-const DEFAULT_COUNT = 12;
+const DEFAULT_INCREMENT = 12;
 
 const Rooms: React.FC<RoomsProps> = () => {
-  const [visibleRoomsCount, setVisibleRoomsCount] = useState(DEFAULT_COUNT);
-  // const handleMoreButtonClick = () => {
-  //   setVisibleRoomsCount((count) => count + DEFAULT_COUNT);
-  // };
-
   const [rooms, setRooms] = useState([]);
+  const [hasMore, setHasMore] = useState(true);
 
   const getNewRooms = useCallback(async () => {
     const fetchedRooms = await roomsList;
-    console.log(fetchedRooms.length);
-
-    setRooms((prevRooms) => {
-      console.log(fetchedRooms.slice(0, prevRooms.length + DEFAULT_COUNT));
-      return fetchedRooms.slice(0, prevRooms.length + DEFAULT_COUNT);
-    });
-  }, [rooms]);
-
-  function handleScroll() {
-    if (
-      window.innerHeight + document.documentElement.scrollTop !==
-      document.documentElement.offsetHeight
-    )
-      return;
-    console.log('Fetch more list items!');
-    getNewRooms();
-  }
-
-  useEffect(() => {
-    window.addEventListener('scroll', handleScroll);
-    return () => window.removeEventListener('scroll', handleScroll);
+    setTimeout(() => {
+      setRooms((prevRooms) => {
+        const updatedList = fetchedRooms.slice(0, prevRooms.length + DEFAULT_INCREMENT);
+        if (updatedList.length === prevRooms.length) setHasMore(false);
+        return updatedList;
+      });
+    }, 1500);
   }, []);
 
   useEffect(() => {
     getNewRooms();
-  }, []);
+  }, [getNewRooms]);
 
   return (
-    // <InfiniteScroll
-    //   dataLength={rooms.length} // This is important field to render the next data
-    //   next={() => getNewRooms()}
-    //   hasMore
-    //   loader={<h4>Loading...</h4>}
-    //   endMessage={
-    //     <p style={{ textAlign: 'center' }}>
-    //       <b>Yay! You have seen it all</b>
-    //     </p>
-    //   }
-    //   style={{ overflow: 'initial' }}
-    //   // below props only if you need pull down functionality
-    // >
-    <S.Rooms>
-      <S.RoomsGrid>
-        {rooms.map((room) => (
-          <S.RoomItem key={room.number}>
-            <Room {...room} />
-          </S.RoomItem>
-        ))}
-      </S.RoomsGrid>
-    </S.Rooms>
-    // </InfiniteScroll>
+    <InfiniteScroll
+      dataLength={rooms.length} // This is important field to render the next data
+      next={() => getNewRooms()}
+      hasMore={hasMore}
+      loader={<h4>Loading...</h4>}
+      endMessage={
+        <p style={{ textAlign: 'center' }}>
+          <b>Yay! You have seen it all</b>
+        </p>
+      }
+      style={{ overflow: 'initial' }}
+    >
+      <S.Rooms>
+        <S.RoomsGrid>
+          {rooms.map((room) => (
+            <S.RoomItem key={room.number}>
+              <Room {...room} />
+            </S.RoomItem>
+          ))}
+        </S.RoomsGrid>
+      </S.Rooms>
+    </InfiniteScroll>
   );
 };
 
