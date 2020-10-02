6d3646e68ddf6fc4911de08a6a1981d987c0bf59
Yuri Osyka <azmodane95@gmail.com>
2020-10-02 20:19:08 +0300

    added declensions

--- components/Comment/utils/getСommentDate.ts
+++ components/Comment/utils/getСommentDate.ts
@@ -1,3 +1,12 @@
+import valueToIndex from './valueToIntex';
+
+const DECLENSION = {
+  seconds: ['секунда', 'секунды', 'секунд'],
+  minutes: ['минута', 'минуты', 'минут'],
+  hours: ['час', 'часа', 'часов'],
+  days: ['день', 'дня', 'дней'],
+};
+
 const options = {
   year: 'numeric',
   month: 'long',
@@ -8,25 +17,45 @@ const options = {
   second: 'numeric',
 };
 
-function getHowMuchTimeHasPassed(date: string): string {
+function getСommentDate(date: string): string {
   const targetDate = new Date(date);
   const currentDate = Date.now();
 
   const diff = currentDate - targetDate.valueOf();
 
   const seconds = diff / 1000;
-  if (seconds < 60) return `${Math.floor(seconds)} секунд назад`;
+  if (seconds < 60) {
+    const roundedSeconds = Math.floor(seconds);
+
+    return `${Math.floor(roundedSeconds)} ${
+      DECLENSION.seconds[valueToIndex(roundedSeconds)]
+    } назад`;
+  }
 
   const minutes = seconds / 60;
-  if (minutes < 60) return `${Math.floor(minutes)} минут назад`;
+  if (minutes < 60) {
+    const roundedMinutes = Math.floor(minutes);
+
+    return `${Math.floor(roundedMinutes)} ${
+      DECLENSION.minutes[valueToIndex(roundedMinutes)]
+    } назад`;
+  }
 
   const hours = minutes / 60;
-  if (hours < 24) return `${Math.floor(hours)} часов назад`;
+  if (hours < 24) {
+    const roundedHours = Math.floor(hours);
+
+    return `${Math.floor(roundedHours)} ${DECLENSION.hours[valueToIndex(roundedHours)]} назад`;
+  }
 
   const days = hours / 24;
-  if (days < 30) return `${Math.floor(days)} дней назад`;
+  if (days < 30) {
+    const roundedDays = Math.floor(days);
+
+    return `${Math.floor(roundedDays)} ${DECLENSION.days[valueToIndex(roundedDays)]} назад`;
+  }
 
   return targetDate.toLocaleDateString('ru-RU', options);
 }
 
-export default getHowMuchTimeHasPassed;
+export default getСommentDate;
