21554e26fd898cfb034e2bd2ec37c0a7f28543fc
Yuri Osyka <azmodane95@gmail.com>
2020-10-06 14:41:17 +0300

    add break point

---
+++ shared/styles/break-points.ts
@@ -0,0 +1,23 @@
+const mediaBreakpointUp = {
+  sm: '(min-width: 576px)',
+  md: '(min-width: 768px)',
+  lg: '(min-width: 992px)',
+  xl: '(min-width: 1200px)',
+};
+
+const mediaBreakpointDown = {
+  xs: '(max-width: 575.98px)',
+  sm: '(max-width: 767.98px)',
+  md: '(max-width: 991.98px)',
+  lg: '(max-width: 1199.98px)',
+};
+
+const mediaBreakpointOnly = {
+  xs: '(max-width: 575.98px)',
+  sm: '(min-width: 576px) and (max-width: 767.98px)',
+  md: '(min-width: 768px) and (max-width: 991.98px)',
+  lg: '(min-width: 992px) and (max-width: 1199.98px)',
+  xl: '(min-width: 1200px)',
+};
+
+export { mediaBreakpointUp, mediaBreakpointDown, mediaBreakpointOnly };
