9c3279daf4c1d2f5e2fd7f5703143e9b0b302a12
Yuri Osyka <azmodane95@gmail.com>
2020-10-07 17:59:54 +0300

    changed gradient vector

--- components/RoomImpression/RoomImpression.tsx
+++ components/RoomImpression/RoomImpression.tsx
@@ -51,19 +51,19 @@ const RoomImpression: React.FC<Props> = ({ title, numberOfRatings }: Props) => {
             paddingAngle={2}
           >
             <defs>
-              <linearGradient id="gradient1" x1="100%" y1="50%" x2="0%" y2="50%">
+              <linearGradient id="gradient1" x1="50%" y1="0%" x2="50%" y2="100%">
                 <stop offset="0" stopColor="#ffe39c" />
                 <stop offset="1" stopColor="#ffba9c" />
               </linearGradient>
-              <linearGradient id="gradient2" x1="100%" y1="50%" x2="0%" y2="50%">
+              <linearGradient id="gradient2" x1="50%" y1="0%" x2="50%" y2="100%">
                 <stop offset="0" stopColor="#bc9cff" />
                 <stop offset="1" stopColor="#8ba4f9" />
               </linearGradient>
-              <linearGradient id="gradient3" x1="100%" y1="50%" x2="0%" y2="50%">
+              <linearGradient id="gradient3" x1="50%" y1="0%" x2="50%" y2="100%">
                 <stop offset="0" stopColor="#6fcf97" />
                 <stop offset="1" stopColor="#66d2ea" />
               </linearGradient>
-              <linearGradient id="gradient4" x1="100%" y1="50%" x2="0%" y2="50%">
+              <linearGradient id="gradient4" x1="50%" y1="0%" x2="50%" y2="100%">
                 <stop offset="0" stopColor="#919191" />
                 <stop offset="1" stopColor="#3d4975" />
               </linearGradient>
