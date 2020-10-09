20ff7acd8a4d3dd15dba5b185b2ab77f617bc2c0
Frostmourne <cozroman@yandex.ru>
2020-10-09 13:54:22 +0300

    fix: fix build error

--- components/SearchRoomForm/SearchRoomForm.tsx
+++ components/SearchRoomForm/SearchRoomForm.tsx
@@ -1,6 +1,6 @@
 import { Form } from 'react-final-form';
 
-import api from 'api/api';
+// import api from 'api/api';
 import ArrowButton from 'components/ArrowButton/ArrowButton';
 import Dropdown from 'components/Dropdown/Dropdown';
 import TimePicker from 'components/TimePicker/TimePicker';
@@ -10,58 +10,58 @@ import * as S from './SearchRoomForm.styles';
 type SearchRoomFormProps = {
   onSubmit?: (e: React.FormEvent<HTMLFormElement>) => void;
 };
-const testRoomFilter = {
-  id: 26,
-  price: { from: 3000, to: 10000 },
-  booked: {
-    from: new Date().getTime(),
-    to: new Date(Date.now() + 1000000000).getTime(),
-  },
-  amenities: {
-    bedrooms: 2,
-    beds: 2,
-    bathrooms: 2,
-  },
-  additionalAmenities: {
-    breakfast: false,
-    desk: false,
-    chair: false,
-    crib: false,
-    tv: false,
-    shampoo: true,
-  },
-  accessibility: {
-    wideCorridor: true,
-    invalidHelper: false,
-  },
-  opportunities: {
-    smoking: true,
-    keepPets: false,
-    largeNumberOfPersons: false,
-  },
-  images: [
-    {
-      alt: 'номер с атмосферой пляжа',
-      url: '/images/apartments/image8.jpg',
-    },
-    {
-      alt: 'уютный двухместный номер',
-      url: '/images/apartments/image12.jpg',
-    },
-    {
-      alt: 'недорогой номер с панорамным остеклением',
-      url: '/images/apartments/image1.jpg',
-    },
-    {
-      alt: 'недорогой номер с панорамным остеклением',
-      url: '/images/apartments/image3.jpg',
-    },
-  ],
-};
-const handleDBRequest = (e) => {
-  e.preventDefault;
-  api.booking.filterRooms(testRoomFilter).then((data) => console.log(data));
-};
+// const testRoomFilter = {
+//   id: 26,
+//   price: { from: 3000, to: 10000 },
+//   booked: {
+//     from: new Date().getTime(),
+//     to: new Date(Date.now() + 1000000000).getTime(),
+//   },
+//   amenities: {
+//     bedrooms: 2,
+//     beds: 2,
+//     bathrooms: 2,
+//   },
+//   additionalAmenities: {
+//     breakfast: false,
+//     desk: false,
+//     chair: false,
+//     crib: false,
+//     tv: false,
+//     shampoo: true,
+//   },
+//   accessibility: {
+//     wideCorridor: true,
+//     invalidHelper: false,
+//   },
+//   opportunities: {
+//     smoking: true,
+//     keepPets: false,
+//     largeNumberOfPersons: false,
+//   },
+//   images: [
+//     {
+//       alt: 'номер с атмосферой пляжа',
+//       url: '/images/apartments/image8.jpg',
+//     },
+//     {
+//       alt: 'уютный двухместный номер',
+//       url: '/images/apartments/image12.jpg',
+//     },
+//     {
+//       alt: 'недорогой номер с панорамным остеклением',
+//       url: '/images/apartments/image1.jpg',
+//     },
+//     {
+//       alt: 'недорогой номер с панорамным остеклением',
+//       url: '/images/apartments/image3.jpg',
+//     },
+//   ],
+// };
+// const handleDBRequest = (e) => {
+//   e.preventDefault;
+//   api.booking.filterRooms(testRoomFilter).then((data) => console.log(data));
+// };
 
 const SearchRoomForm: React.FC<SearchRoomFormProps> = ({ onSubmit }: SearchRoomFormProps) => {
   const handleFormSubmit = (e: React.FormEvent<HTMLFormElement>) => {
@@ -111,7 +111,7 @@ const SearchRoomForm: React.FC<SearchRoomFormProps> = ({ onSubmit }: SearchRoomF
                 ]}
               />
             </S.DropdownWrapper>
-            <ArrowButton isLink={false} isFilled onClick={handleDBRequest} type="button">
+            <ArrowButton isLink={false} isFilled type="button">
               Подобрать номер
             </ArrowButton>
           </form>
