febc9a3fe70c9084cf63f7d5276f2719da936bc8
Yuri Osyka <azmodane95@gmail.com>
2020-10-27 16:49:55 +0300

    added birthday and gender update

--- components/PersonalInfoPage/components/EditPersonalInfo/components/EditBirthday/EditBirthday.tsx
+++ components/PersonalInfoPage/components/EditPersonalInfo/components/EditBirthday/EditBirthday.tsx
@@ -1,16 +1,49 @@
 import { Form, Field } from 'react-final-form';
+import { connect } from 'react-redux';
 
+import { User } from 'api/Firebase/modules/Authentication/types';
 import Button from 'components/Button/Button';
 import Input from 'components/Input/Input';
+import PopUpNotification from 'components/PopUpNotification/PopUpNotification';
+import { AppState } from 'redux/store.types';
+import {
+  updateAdditionalUserDataRequest,
+  updateAdditionalUserDataCompleted,
+} from 'redux/UpdateAdditionalUserData/redux/actions';
 import { dateValidator, dateFormatMask } from 'shared/helpers/validators';
 
+interface IStateProps {
+  isCompleted: boolean;
+  statusText: string;
+}
+
+const mapState = (state: AppState): IStateProps => state.updateAdditionalUserDataReducer;
+
+const mapDispatch = {
+  startUpdateAdditionalUserDataProcess: updateAdditionalUserDataRequest,
+  stopUpdateAdditionalUserDataProcess: updateAdditionalUserDataCompleted,
+};
+
 type Props = {
+  user: User;
+  birthday: string;
+} & ReturnType<typeof mapState> &
+  typeof mapDispatch;
+
+type FormData = {
   birthday: string;
 };
 
-const EditBirthday = ({ birthday }: Props): JSX.Element => {
-  const onSubmit = (data) => {
-    data;
+const EditBirthday = ({
+  user,
+  birthday,
+  isCompleted,
+  statusText,
+  startUpdateAdditionalUserDataProcess,
+  stopUpdateAdditionalUserDataProcess,
+}: Props): JSX.Element => {
+  const onSubmit = ({ birthday: newBirthday }: FormData) => {
+    startUpdateAdditionalUserDataProcess({ user, data: { birthDate: newBirthday } });
   };
 
   return (
@@ -18,24 +51,32 @@ const EditBirthday = ({ birthday }: Props): JSX.Element => {
       initialValues={{ birthday }}
       onSubmit={onSubmit}
       render={({ handleSubmit }) => (
-        <form onSubmit={handleSubmit}>
-          <Field
-            name="birthday"
-            render={({ input }) => (
-              <Input
-                {...input}
-                placeholder="ДД.ММ.ГГГГ"
-                validators={[dateValidator]}
-                mask={dateFormatMask}
-              />
-            )}
-          />
-          <Button isFlat isFilled>
-            Сохранить
-          </Button>
-        </form>
+        <>
+          <form onSubmit={handleSubmit}>
+            <Field
+              name="birthday"
+              render={({ input }) => (
+                <Input
+                  {...input}
+                  placeholder="ДД.ММ.ГГГГ"
+                  validators={[dateValidator]}
+                  mask={dateFormatMask}
+                />
+              )}
+            />
+            <Button isFlat isFilled>
+              Сохранить
+            </Button>
+          </form>
+          {isCompleted && (
+            <PopUpNotification
+              message={statusText}
+              onConfirmButtonClick={stopUpdateAdditionalUserDataProcess}
+            />
+          )}
+        </>
       )}
     />
   );
 };
-export default EditBirthday;
+export default connect(mapState, mapDispatch)(EditBirthday);
--- components/PersonalInfoPage/components/EditPersonalInfo/components/EditGender/EditGender.tsx
+++ components/PersonalInfoPage/components/EditPersonalInfo/components/EditGender/EditGender.tsx
@@ -1,38 +1,76 @@
 import { Form } from 'react-final-form';
+import { connect } from 'react-redux';
 
+import { User } from 'api/Firebase/modules/Authentication/types';
 import Button from 'components/Button/Button';
+import PopUpNotification from 'components/PopUpNotification/PopUpNotification';
 import RadioButton from 'components/RadioButton/RadioButton';
+import {
+  updateAdditionalUserDataRequest,
+  updateAdditionalUserDataCompleted,
+} from 'redux/UpdateAdditionalUserData/redux/actions';
 
 import * as S from './EditGender.styles';
 
+interface IStateProps {
+  isCompleted: boolean;
+  statusText: string;
+}
+
+const mapState = (state: AppState): IStateProps => state.updateAdditionalUserDataReducer;
+
+const mapDispatch = {
+  startUpdateAdditionalUserDataProcess: updateAdditionalUserDataRequest,
+  stopUpdateAdditionalUserDataProcess: updateAdditionalUserDataCompleted,
+};
+
 type Props = {
+  user: User;
+  gender: string;
+} & ReturnType<typeof mapState> &
+  typeof mapDispatch;
+
+type FormData = {
   gender: string;
 };
 
-const EditGender = ({ gender }: Props): JSX.Element => {
-  const onSubmit = (data) => {
-    data;
+const EditGender = ({
+  user,
+  gender,
+  isCompleted,
+  statusText,
+  startUpdateAdditionalUserDataProcess,
+  stopUpdateAdditionalUserDataProcess,
+}: Props): JSX.Element => {
+  const onSubmit = ({ gender: newGender }: FormData) => {
+    startUpdateAdditionalUserDataProcess({ user, data: { gender: newGender } });
   };
-
-  const mapGender = { Мужчина: 'man', Женщина: 'woman' };
 
   return (
     <Form
-      initialValues={{ gender: mapGender[gender] }}
+      initialValues={{ gender }}
       onSubmit={onSubmit}
       render={({ handleSubmit }) => (
-        <form onSubmit={handleSubmit}>
-          <S.Gender>
-            <RadioButton name="gender" value="man" label="Мужчина" />
-            <RadioButton name="gender" value="wonman" label="Женщина" />
-          </S.Gender>
-          <Button isFlat isFilled>
-            Сохранить
-          </Button>
-        </form>
+        <>
+          <form onSubmit={handleSubmit}>
+            <S.Gender>
+              <RadioButton name="gender" value="Мужчина" label="Мужчина" />
+              <RadioButton name="gender" value="Женщина" label="Женщина" />
+            </S.Gender>
+            <Button isFlat isFilled>
+              Сохранить
+            </Button>
+          </form>
+          {isCompleted && (
+            <PopUpNotification
+              message={statusText}
+              onConfirmButtonClick={stopUpdateAdditionalUserDataProcess}
+            />
+          )}
+        </>
       )}
     />
   );
 };
 
-export default EditGender;
+export default connect(mapState, mapDispatch)(EditGender);
