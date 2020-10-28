761d783762fe54362611530b85eb96d863ae7759
Yuri Osyka <azmodane95@gmail.com>
2020-10-28 09:43:53 +0300

    added update of subscription data

--- components/NotificationsPage/components/MainContent/MainContent.tsx
+++ components/NotificationsPage/components/MainContent/MainContent.tsx
@@ -1,11 +1,55 @@
+import { useCallback, useEffect } from 'react';
+import { connect } from 'react-redux';
+
+import { AdditionalUserInformation } from 'api/entities/types';
+import { User } from 'api/Firebase/modules/Authentication/types';
+import { getAdditionalUserDataRequest } from 'redux/GetAdditionalUserData/redux/actions';
+import { AppState } from 'redux/store.types';
+
 import Subscriptions from '../Subscriptions/Subscriptions';
 import * as S from './MainContent.styles';
 
-const MainContent = (): JSX.Element => (
-  <S.MainContent>
-    <S.Title>Уведомления</S.Title>
-    <Subscriptions />
-  </S.MainContent>
-);
+interface IStateProps {
+  user: User;
+  isCompleted: boolean;
+  additionalUserData: AdditionalUserInformation;
+}
 
-export default MainContent;
+const mapState = (state: AppState): IStateProps => ({
+  user: state.authReducer.user,
+  ...state.getAdditionalUserDataReducer,
+});
+
+const mapDispatch = {
+  startGetAdditionalUserDataProcess: getAdditionalUserDataRequest,
+};
+
+type Props = ReturnType<typeof mapState> & typeof mapDispatch;
+
+const MainContent = ({
+  user,
+  isCompleted,
+  additionalUserData,
+  startGetAdditionalUserDataProcess,
+}: Props): JSX.Element => {
+  const handleGetAdditionalUserData = useCallback(
+    (currentUser) => {
+      startGetAdditionalUserDataProcess(currentUser);
+    },
+    [startGetAdditionalUserDataProcess],
+  );
+
+  useEffect(() => {
+    handleGetAdditionalUserData(user);
+  }, [handleGetAdditionalUserData, user]);
+
+  return (
+    <S.MainContent>
+      <S.Title>Уведомления</S.Title>
+      {isCompleted && (
+        <Subscriptions user={user} receiveOffers={additionalUserData.receiveOffers} />
+      )}
+    </S.MainContent>
+  );
+};
+export default connect(mapState, mapDispatch)(MainContent);
--- components/NotificationsPage/components/Subscriptions/Subscriptions.tsx
+++ components/NotificationsPage/components/Subscriptions/Subscriptions.tsx
@@ -1,33 +1,85 @@
 import { Form } from 'react-final-form';
+import { connect } from 'react-redux';
 
+import { User } from 'api/Firebase/modules/Authentication/types';
 import Button from 'components/Button/Button';
+import PopUpNotification from 'components/PopUpNotification/PopUpNotification';
 import Toggle from 'components/Toggle/Toggle';
+import { AppState } from 'redux/store.types';
+import {
+  updateAdditionalUserDataRequest,
+  updateAdditionalUserDataCompleted,
+} from 'redux/UpdateAdditionalUserData/redux/actions';
 
 import * as S from './Subscriptions.styles';
 
-const onSubmit = (data) => {
-  data;
+interface IStateProps {
+  user: User;
+  isCompleted: boolean;
+  statusText: string;
+}
+
+const mapState = (state: AppState): IStateProps => ({
+  user: state.authReducer.user,
+  ...state.updateAdditionalUserDataReducer,
+});
+
+const mapDispatch = {
+  startUpdateAdditionalUserDataProcess: updateAdditionalUserDataRequest,
+  stopUpdateAdditionalUserDataProcess: updateAdditionalUserDataCompleted,
 };
 
-const Subscriptions = (): JSX.Element => (
-  <S.Subscriptions>
-    <S.Title>Новостные рассылки</S.Title>
-    <Form
-      onSubmit={onSubmit}
-      render={({ handleSubmit }) => (
-        <form onSubmit={handleSubmit}>
-          <S.List>
-            <S.Item>
-              <Toggle name="specialOffers" label="Получать спецпредложения" />
-            </S.Item>
-          </S.List>
-          <Button isFlat isFilled>
-            Сохранить
-          </Button>
-        </form>
-      )}
-    />
-  </S.Subscriptions>
-);
+type Props = {
+  user: User;
+  receiveOffers: boolean;
+} & ReturnType<typeof mapState> &
+  typeof mapDispatch;
 
-export default Subscriptions;
+type FormData = {
+  specialOffers: boolean;
+};
+
+const Subscriptions = ({
+  user,
+  receiveOffers,
+  isCompleted,
+  statusText,
+  startUpdateAdditionalUserDataProcess,
+  stopUpdateAdditionalUserDataProcess,
+}: Props): JSX.Element => {
+  const onSubmit = ({ specialOffers }: FormData) => {
+    startUpdateAdditionalUserDataProcess({ user, data: { receiveOffers: specialOffers } });
+  };
+
+  return (
+    <S.Subscriptions>
+      <S.Title>Новостные рассылки</S.Title>
+      <Form
+        initialValues={{ specialOffers: receiveOffers }}
+        onSubmit={onSubmit}
+        render={({ handleSubmit }) => (
+          <>
+            <form onSubmit={handleSubmit}>
+              <S.List>
+                <S.Item>
+                  <Toggle name="specialOffers" label="Получать спецпредложения" />
+                </S.Item>
+              </S.List>
+              <Button isFlat isFilled>
+                Сохранить
+              </Button>
+            </form>
+            {isCompleted && (
+              <PopUpNotification
+                message={statusText}
+                onConfirmButtonClick={stopUpdateAdditionalUserDataProcess}
+              />
+            )}
+          </>
+        )}
+      />
+    </S.Subscriptions>
+  );
+};
+
+export default connect(mapState, mapDispatch)(Subscriptions);
