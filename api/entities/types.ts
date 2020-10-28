b08ee042beec34bf4179c09ec1d3559752f313a5
Yuri Osyka <azmodane95@gmail.com>
2020-10-28 13:16:43 +0300

    fixed type

--- api/entities/types.ts
+++ api/entities/types.ts
@@ -76,7 +76,7 @@ export type ProfileData = {
 
 export type AdditionalUserInformation = {
   birthDate: string;
-  gender: 'Мужчина' | 'Женщина';
+  gender: 'female' | 'male';
   receiveOffers: boolean;
 };
 
