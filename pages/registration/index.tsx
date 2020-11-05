903cae44bab833247cb796b5360d8139c7c212cc
Frostmourne <cozroman@yandex.ru>
2020-11-05 15:00:26 +0300

    feat: add titles and meta info

--- pages/registration/index.tsx
+++ pages/registration/index.tsx
@@ -1,5 +1,19 @@
+import Head from 'next/head';
+
 import RegistrationPage from 'components/RegistrationPage/RegistrationPage';
 
-const Registration: React.FC = (): JSX.Element => <RegistrationPage />;
+const Registration: React.FC = (): JSX.Element => (
+  <>
+    <Head>
+      <title>Toxin | Регистрация</title>
+      <meta name="description" content="Регистрация для бронирования номеров в нашем отеле" />
+      <meta
+        name="keywords"
+        content="отель, номер, регистрация, авторизация, забронировать, войти"
+      />
+    </Head>
+    <RegistrationPage />
+  </>
+);
 
 export default Registration;
