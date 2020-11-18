6d83fc49d8810571521450d238c320b37f6d9778
frostm0urne <cozroman@yandex.ru>
2020-11-18 10:54:45 +0300

    fix: fix app crash in safari

--- utils/number.utils.ts
+++ utils/number.utils.ts
@@ -1,19 +1,33 @@
-export function formatNumber(target: number, currencyCode = 'RUB', separator = '\u00A0'): string {
-  const initialParts = new Intl.NumberFormat('ru', {
+export function formatNumber(
+  target: number,
+  currencyCode = 'RUB',
+  separator = '\u00A0',
+  locale = 'ru',
+): string {
+  if (new Intl.NumberFormat().formatToParts) {
+    const initialParts = new Intl.NumberFormat(locale, {
+      style: 'currency',
+      currency: currencyCode,
+      minimumFractionDigits: 0,
+      maximumFractionDigits: 0,
+    }).formatToParts(target);
+    const formattedParts = initialParts.map(({ type, value }) => {
+      switch (type) {
+        case 'group':
+          return separator;
+        case 'literal':
+          return '';
+        default:
+          return value;
+      }
+    });
+    return formattedParts.reduce((acc, part) => acc + part, '');
+  }
+
+  return target.toLocaleString(locale, {
     style: 'currency',
     currency: currencyCode,
     minimumFractionDigits: 0,
     maximumFractionDigits: 0,
-  }).formatToParts(target);
-  const formattedParts = initialParts.map(({ type, value }) => {
-    switch (type) {
-      case 'group':
-        return separator;
-      case 'literal':
-        return '';
-      default:
-        return value;
-    }
   });
-  return formattedParts.reduce((acc, part) => acc + part, '');
 }
